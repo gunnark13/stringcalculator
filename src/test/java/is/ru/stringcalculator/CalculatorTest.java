4bef3c228bd2ae7e4f715b22c458bdb1c6aa3c9c
Gunnar Orri <gunnark13@ru.is>
2014-10-12 17:53:13 +0000

    Added the test testNegativeNumbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -29,4 +29,9 @@ public class CalculatorTest {
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 
+    	@Test
+    public void testNegativeNumbers(){
+    	assertEquals(2, Calculator.add("1,-2,3"));
+    }
+
 }
