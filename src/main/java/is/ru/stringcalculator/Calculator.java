ab0ba066e933ea5f9fc976222ddf2a16882f3ec9
Gunnar Orri <gunnark13@ru.is>
2014-10-13 21:35:13 +0000

    Implemented the splitNumbers method so that it handles new lines between numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
 		if(text.equals("")){
 			return 0; // empty comment for first commit
 		}
-		else if(text.contains(",")){
+		else if(text.contains(",") || text.matches("\n")){
 			return sum(splitNumbers(text));
 		}
 		else
@@ -18,7 +18,7 @@ public class Calculator {
 	}
 
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
+	    return numbers.split(",|\\n");
 	}
       
     private static int sum(String[] numbers){
