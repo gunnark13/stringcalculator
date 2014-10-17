833c99db6b502dce2ef6a2cc2758164d4806e612
Gunnar Orri <gunnark13@ru.is>
2014-10-17 15:02:14 +0000

    Added the test multipleDelimiters

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -58,4 +58,9 @@ public class CalculatorTest {
     public void numbersBiggerThan1000(){
     	assertEquals(2, Calculator.add("1001,2"));
     }
+
+   	@Test
+    public void multipleDelimiters(){
+    	assertEquals(6, Calculator.add("//[***]\n1***2***3"));
+    }
 }
