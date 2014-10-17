ff9af5721065859fb90ea60fb8b4caa76d99acc0
Gunnar Orri <gunnark13@ru.is>
2014-10-17 20:58:26 +0000

    A bit of refactoring and commenting, everything working fine but delimiterOfAnyLength has not yet been implemented

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,9 +3,7 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		if(text.equals("")){
-			return 0; // empty comment for first commit
-		}
+		if(text.isEmpty()){return 0;}
 		else if(text.contains(",") || text.contains("\n")){
 			return sum(splitNumbers(text));
 		}
@@ -13,9 +11,8 @@ public class Calculator {
 			String delimiter = text.substring(2,3);
 			return sum(splitNumbers(text));
 		}
-		else{
-			return 1;
-		}
+		else{return 1;} 
+			
 	}
 
 	private static int toInt(String number){
@@ -24,19 +21,23 @@ public class Calculator {
 
 	private static String[] splitNumbers(String numbers){
 	    if(numbers.startsWith("//")){
-	    	// we find the delimiter, and put it in a string.
-			String delimiter = numbers.substring(2,3);
-			// we make a new regex delimiter like [,\n;] if the delimiter is ;
+	    	String delimiter = "";
+	    	int indexOfNewline = numbers.indexOf("\n");
+
+	    	// if only one delimiter, we know newline comes as the third character.
+	    	if(numbers.indexOf("\n") == 3){
+		    	// we find the delimiter, and put it in a string.
+				delimiter = numbers.substring(2,3);
+				// we make a new regex delimiter like [,\n;] if the delimiter is ;
 				delimiter = "[,\n" + delimiter + "]";
-			// find the index of the first newline
-				int index = numbers.indexOf("\n");
-			// trim the first newline and everything in front of it
-				numbers = numbers.substring(index + 1);
-			// split the string with our delimiter
-			return numbers.split(delimiter);
-    	}
-    	else
-    		return numbers.split(",|\n");
+				// find the index of the first newline
+				// trim the first newline and everything in front of it
+				numbers = numbers.substring(indexOfNewline + 1);
+				// split the string with our delimiter
+				return numbers.split(delimiter);
+			}	
+    	}			
+    	return numbers.split(",|\n");
 	}
       
     private static int sum(String[] numbers){
@@ -48,14 +49,12 @@ public class Calculator {
         		String illegalMessage = IllegalArgumentMessage(numbers);
         		throw new IllegalArgumentException(illegalMessage);
         	}
-        	else{
-        		if(toInt(number) > 1000) {
+        	else
+        		if(toInt(number) > 1000){
         			total = total;
         		}
-        		else{		    
+        		else		    
         			total += toInt(number);
-        		}
-			}
 		}
 		return total;
     }
